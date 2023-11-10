63699b7c7d55878586964663efe598349b2ea04d
(Student  C00301460) Conor Foley <C00301460@setu.ie>
2023-11-10 11:51:14 +0000

    Added the letter Y

--- PetesWorld/main.cpp
+++ PetesWorld/main.cpp
@@ -23,7 +23,7 @@ int main()
 		std::cout << "You need to select a number between 1 and 5" << std::endl;
 		return 0;
 	}
-	std::cout << "Pick a letter from A to A or Z to Z" << std::endl; // change this line each iteration
+	std::cout << "Pick a letter from A to A or Y to Z" << std::endl; // change this line each iteration
 	std::cin >> letter;
 	if (letter < 'A' || letter > 'Z') // not a capital letter
 	{ 
@@ -56,6 +56,32 @@ int main()
 			std::cout << "Australia, Antarctica, Aruba, Anguilla Leeward Islands, Antigua and Barbuda, American Samoa" << std::endl;
 		}
 	}	
+	
+	if (letter == 'y' || letter == 'Y') // written by Conor
+	{
+		if (region == 1)
+		{
+			std::cout << "There are no Ys in the Americas" << std::endl; //Default negative response
+		}
+		if (region == 2)
+		{
+			std::cout << "There are no Ys in Europe" << std::endl;
+		}
+		if (region == 3)
+		{
+			std::cout << "There are no Ys in Africa" << std::endl;
+		}
+		if (region == 4)
+		{
+			std::cout << "Yemen" << std::endl;
+		}
+		if (region == 5)
+		{
+			std::cout << "There are no Ys in the rest of the world" << std::endl;
+		}
+	}
+	
+	
 	if (letter == 'z' || letter == 'Z') // written by Pete
 	{
 		if (region == 1)
